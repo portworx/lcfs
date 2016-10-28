707d072f9177aa0cd523382ce8e689f5280555c7
Jobi <jobi@portworx.com>
2016-10-28 08:10:29 -0700

    let fuse take care of arguments

--- fs/dfs.c
+++ fs/dfs.c
@@ -1,7 +1,5 @@
 #include "includes.h"
 
-#define ARGC    4
-
 static struct gfs *gfs;
 extern struct fuse_lowlevel_ops dfs_ll_oper;
 
@@ -14,26 +12,12 @@ getfs() {
 
 /* Process fuse requests */
 static int
-dfs_loop(struct fuse_session *se, bool daemon) {
+dfs_loop(struct fuse_session *se, int foreground) {
     int err = -1;
-    pid_t pid;
-    int fd;
 
-    /* Detach from the terminal */
-    if (daemon) {
-        pid = fork();
-        if (pid > 0) {
-            exit(0);
-        }
-        setsid();
-        chdir("/");
-        fd = open("/dev/null", O_RDWR);
-        dup2(fd, STDIN_FILENO);
-        dup2(fd, STDOUT_FILENO);
-        dup2(fd, STDERR_FILENO);
-    }
     if (fuse_set_signal_handlers(se) != -1) {
         fuse_session_add_chan(se, gfs->gfs_ch);
+        fuse_daemonize(foreground);
         err = fuse_session_loop_mt(se);
         fuse_remove_signal_handlers(se);
         fuse_session_remove_chan(gfs->gfs_ch);
@@ -41,17 +25,22 @@ dfs_loop(struct fuse_session *se, bool daemon) {
     return err;
 }
 
+/* Display usage */
+static void
+usage(char *prog) {
+    printf("usage: %s <device> <mnt> [-d] [-f]\n", prog);
+}
+
 /* Mount the specified device and start serving requests */
 int
 main(int argc, char *argv[]) {
+    int i, err = -1, foreground;
     struct fuse_session *se;
+    char *arg[argc + 1];
     char *mountpoint;
-    char *arg[ARGC];
-    int err = -1;
-    bool daemon;
 
-    if (argc != 3) {
-        printf("%s: device mnt\n", argv[0]);
+    if ((argc < 3) || (argc > 5)) {
+        usage(argv[0]);
         exit(EINVAL);
     }
     err = dfs_mount(argv[1], &gfs);
@@ -68,24 +57,28 @@ main(int argc, char *argv[]) {
             "subtype=dfs,fsname=%s,big_writes,noatime,"
             "splice_move,splice_read,splice_write",
             argv[1]);
-    if (ARGC >= 5) {
-        arg[4] = "-d";
-        daemon = false;
-    } else {
-        /* XXX Add command line argument for disabling this */
-        daemon = true;
+    for (i = 3; i < argc; i++) {
+        arg[i + 1] = argv[i];
     }
 
-    struct fuse_args args = FUSE_ARGS_INIT(ARGC, arg);
-    if (fuse_parse_cmdline(&args, &mountpoint, NULL, NULL) != -1 &&
-        (gfs->gfs_ch = fuse_mount(mountpoint, &args)) != NULL) {
+    struct fuse_args args = FUSE_ARGS_INIT(argc + 1, arg);
+    if ((fuse_parse_cmdline(&args, &mountpoint, NULL, &foreground) != -1) &&
+        ((gfs->gfs_ch = fuse_mount(mountpoint, &args)) != NULL)) {
         se = fuse_lowlevel_new(&args, &dfs_ll_oper,
                                sizeof(dfs_ll_oper), gfs);
+        free(arg[3]);
         if (se) {
-            err = dfs_loop(se, daemon);
+            err = dfs_loop(se, foreground);
             fuse_session_destroy(se);
+        } else {
+            err = EINVAL;
+            usage(argv[0]);
         }
         fuse_unmount(mountpoint, gfs->gfs_ch);
+    } else {
+        free(arg[3]);
+        usage(argv[0]);
+        err = EINVAL;
     }
     fuse_opt_free_args(&args);
     return err ? 1 : 0;
