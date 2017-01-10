3934e391554d32af0aa94635d0f9be075026cd55
Jobi <jobi@portworx.com>
2017-01-10 10:04:17 -0800

    A script to configure docker with lcfs and px-graph on Ubuntu

---
+++ setup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -x
+
+#Stop docker
+#systemctl stop docker
+service docker stop
+pkill dockerd
+sleep 3
+fusermount -u /lcfs
+fusermount -u /var/lib/docker
+umount -f /lcfs /var/lib/docker
+rm -fr /var/lib/docker /lcfs
+
+#Choose a device for lcfs
+export DEVICE=/dev/sdb
+dd if=/dev/zero of=$DEVICE count=1 bs=4096
+
+export GOPATH=$HOME/portworx
+rm -fr $GOPATH
+mkdir -p $GOPATH 2>/dev/null
+
+apt-get update
+
+#Install wget
+apt-get install -y wget
+
+#Install go
+#wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
+#tar -C /usr/local -xzvf go1.7.4.linux-amd64.tar.gz
+#rm go1.7.4.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+#Install build tools
+#apt-get install -y build-essential libcurl4-openssl-dev libxml2-dev mime-support
+#yum install gcc libstdc++-devel gcc-c++ curl-devel libxml2-devel openssl-devel mailcap
+
+#Install tcmalloc
+apt-get install -y libgoogle-perftools-dev
+#yum install gperftools
+
+#Install FUSE 2.9.7
+wget https://github.com/libfuse/libfuse/releases/download/fuse-2.9.7/fuse-2.9.7.tar.gz
+tar -xzvf fuse-2.9.7.tar.gz
+cd fuse-2.9.7
+./configure
+make -j8
+make install
+rm -fr fuse-2.9.7*
+
+# Build lcfs
+go get -d github.com/portworx/px-graph/...
+cd $GOPATH/src/github.com/portworx/px-graph/lcfs
+make
+
+#Mount lcfs
+mkdir -p /var/lib/docker /lcfs
+$GOPATH/src/github.com/portworx/px-graph/lcfs/lcfs $DEVICE /var/lib/docker /lcfs -f &
+sleep 3
+
+#Restart docker
+dockerd -s vfs &
+
+#Create and enable Px-Graph plugin
+cd $GOPATH/src/github.com/portworx/px-graph/plugin
+./setup.sh
+
+#Restart docker with Px-Graph
+pkill dockerd
+sleep 3
+dockerd -s portworx/px-graph &
+sleep 3
+docker info
+
+#pkill dockerd
+#fusermount -u /lcfs
