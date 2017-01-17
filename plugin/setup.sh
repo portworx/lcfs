c4dd8fe7f0c089293a47642b686b7d803c445fd3
Aditya Dani <aditya@portworx.com>
2017-01-16 17:23:50 -0800

    Modify setup.sh to handle existing plugins and rootfsimages

--- plugin/setup.sh
+++ plugin/setup.sh
@@ -1,14 +1,25 @@
-#!/bin/bash -xe
+#!/bin/bash
 set -x
 
 make clean
 make
 sudo rm -fr rootfs 2>/dev/null
 
+docker image inspect rootfsimage
+if [ $? -eq 0 ]; then
+    docker rmi rootfsimage
+fi
+
 docker build -t rootfsimage .
 id=$(docker create rootfsimage)
 mkdir rootfs
 docker export "$id" | tar -x -C rootfs/
+
+docker plugin inspect portworx/lcfs
+if [ $? -eq 0 ]; then
+    docker plugin disable portworx/lcfs
+    docker plugin rm portworx/lcfs
+fi
 sudo docker plugin create portworx/lcfs .
 docker plugin enable portworx/lcfs
 
